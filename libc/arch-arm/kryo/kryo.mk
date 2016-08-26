ca8975f1a0f325b91826ba7792b7bb83e03dc368
Jake Weinstein <xboxlover360@gmail.com>
2016-08-25 20:03:25 -0400

    libc: optimize memcpy and memmove for 32-bit kryo
    * Memcpy is based on Scorpion due to Qualcomm's
      128-bit cache line size optimizations
    
    * Memmove is identical to Denver, but uses 128-bit
      cache line size.
    
    * PLDOFFSET and PLDSIZE taken from ARM64 Kryo memcpy routine
    
    * Tested on OnePlus 3 with MSM8996
    
    Before:
    BM_string_memcpy/8                          1000k          9    0.855 GiB/s
    BM_string_memcpy/64                         1000k          7    8.550 GiB/s
    BM_string_memcpy/512                        1000k         17   28.832 GiB/s
    BM_string_memcpy/1024                         50M         37   27.053 GiB/s
    BM_string_memcpy/8Ki                           5M        313   26.092 GiB/s
    BM_string_memcpy/16Ki                          5M        608   26.911 GiB/s
    BM_string_memcpy/32Ki                       1000k       1224   26.756 GiB/s
    BM_string_memcpy/64Ki                       1000k       2460   26.632 GiB/s
    
    BM_string_memmove/8                         1000k          9    0.853 GiB/s
    BM_string_memmove/64                        1000k         11    5.747 GiB/s
    BM_string_memmove/512                       1000k         20   24.840 GiB/s
    BM_string_memmove/1024                        50M         40   25.473 GiB/s
    BM_string_memmove/8Ki                          5M        319   25.680 GiB/s
    BM_string_memmove/16Ki                         5M        617   26.523 GiB/s
    BM_string_memmove/32Ki                      1000k       1343   24.381 GiB/s
    BM_string_memmove/64Ki                      1000k       2608   25.125 GiB/s
    
    After:
    BM_string_memcpy/8                          1000k          6    1.221 GiB/s
    BM_string_memcpy/64                         1000k          7    8.601 GiB/s
    BM_string_memcpy/512                        1000k         18   27.405 GiB/s
    BM_string_memcpy/1024                         50M         33   30.354 GiB/s
    BM_string_memcpy/8Ki                          10M        255   32.020 GiB/s
    BM_string_memcpy/16Ki                          5M        527   31.055 GiB/s
    BM_string_memcpy/32Ki                       1000k       1186   27.615 GiB/s
    BM_string_memcpy/64Ki                       1000k       2365   27.710 GiB/s
    
    BM_string_memmove/8                         1000k          7    1.007 GiB/s
    BM_string_memmove/64                        1000k          9    6.425 GiB/s
    BM_string_memmove/512                       1000k         21   24.302 GiB/s
    BM_string_memmove/1024                        50M         37   27.002 GiB/s
    BM_string_memmove/8Ki                         10M        284   28.759 GiB/s
    BM_string_memmove/16Ki                         5M        585   27.959 GiB/s
    BM_string_memmove/32Ki                      1000k       1335   24.542 GiB/s
    BM_string_memmove/64Ki                      1000k       2651   24.719 GiB/s
    
    Change-Id: Id7a9c37ef75a306dd5cf8d374d79d0fe83f8a3ba

--- libc/arch-arm/kryo/kryo.mk
+++ libc/arch-arm/kryo/kryo.mk
@@ -1,19 +1,15 @@
 libc_bionic_src_files_arm += \
     arch-arm/krait/bionic/strcmp.S \
-    arch-arm/krait/bionic/__strcat_chk.S \
-    arch-arm/krait/bionic/__strcpy_chk.S \
+    bionic/__strcat_chk.cpp \
+    bionic/__strcpy_chk.cpp \
 
 # Memset from A53/A7
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a7/bionic/memset.S
 
-# Memmove from Denver
 libc_bionic_src_files_arm += \
-    arch-arm/denver/bionic/memmove.S
-
-# Krait optimized memcpy brings the best metrics on kryo
-libc_bionic_src_files_arm += \
-    arch-arm/krait/bionic/memcpy.S
+    arch-arm/kryo/bionic/memcpy.S \
+    arch-arm/kryo/bionic/memmove.S
 
 # Use cortex-a15 versions of strcat/strcpy/strlen
 libc_bionic_src_files_arm += \
@@ -25,5 +21,3 @@ libc_bionic_src_files_arm += \
 libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memchr.S \
     arch-arm/generic/bionic/memcmp.S \
-
-
