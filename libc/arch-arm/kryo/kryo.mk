dfca468f8bb42855e36bd50e58fc5682edbb24da
Jake Weinstein <xboxlover360@gmail.com>
2016-08-25 20:03:25 -0400

    libc: optimize memcpy and memmove for 32-bit kryo
    * Memcpy is based on Scorpion due to Qualcomm's
      128-bit cache line size optimizations
    
    * Memmove is identical to Denver, but uses 128-bit
      cache line size.
    
    * PLDOFFSET and PLDSIZE taken from ARM64 Kryo memcpy routine
    
    * Tested on OnePlus 3 with MSM8996
    
    Before:
    BM_string_memcpy/8                          1000k          8    0.934 GiB/s
    BM_string_memcpy/64                         1000k         11    5.785 GiB/s
    BM_string_memcpy/512                        1000k         25   19.918 GiB/s
    BM_string_memcpy/1024                         50M         42   23.938 GiB/s
    BM_string_memcpy/8Ki                          10M        473   17.291 GiB/s
    BM_string_memcpy/16Ki                          5M        565   28.976 GiB/s
    BM_string_memcpy/32Ki                       1000k       1105   29.631 GiB/s
    BM_string_memcpy/64Ki                       1000k       2194   29.864 GiB/s
    
    BM_string_memmove/8                         1000k          9    0.816 GiB/s
    BM_string_memmove/64                        1000k         12    5.126 GiB/s
    BM_string_memmove/512                       1000k         27   18.544 GiB/s
    BM_string_memmove/1024                        50M         46   22.108 GiB/s
    BM_string_memmove/8Ki                          5M        323   25.323 GiB/s
    BM_string_memmove/16Ki                         5M        641   25.544 GiB/s
    BM_string_memmove/32Ki                      1000k       1235   26.523 GiB/s
    BM_string_memmove/64Ki                      1000k       2428   26.984 GiB/s
    
    After:
    BM_string_memcpy/8                          1000k          6    1.221 GiB/s
    BM_string_memcpy/64                         1000k          7    8.601 GiB/s
    BM_string_memcpy/512                        1000k         18   27.405 GiB/s
    BM_string_memcpy/1024                         50M         33   30.354 GiB/s
    BM_string_memcpy/8Ki                          10M        255   32.020 GiB/s
    BM_string_memcpy/16Ki                          5M        527   31.055 GiB/s
    BM_string_memcpy/32Ki                       1000k       1186   27.615 GiB/s
    BM_string_memcpy/64Ki                       1000k       2365   27.710 GiB/s
    
    BM_string_memmove/8                         1000k          7    1.007 GiB/s
    BM_string_memmove/64                        1000k          9    6.425 GiB/s
    BM_string_memmove/512                       1000k         21   24.302 GiB/s
    BM_string_memmove/1024                        50M         37   27.002 GiB/s
    BM_string_memmove/8Ki                         10M        284   28.759 GiB/s
    BM_string_memmove/16Ki                         5M        585   27.959 GiB/s
    BM_string_memmove/32Ki                      1000k       1335   24.542 GiB/s
    BM_string_memmove/64Ki                      1000k       2651   24.719 GiB/s
    
    Change-Id: Id7a9c37ef75a306dd5cf8d374d79d0fe83f8a3ba

---
+++ libc/arch-arm/kryo/kryo.mk
@@ -0,0 +1,28 @@
+libc_openbsd_src_files_exclude_arm += \
+    upstream-openbsd/lib/libc/string/memmove.c \
+    upstream-openbsd/lib/libc/string/stpcpy.c \
+    upstream-openbsd/lib/libc/string/stpcpy.c \
+    upstream-openbsd/lib/libc/string/strcat.c \
+
+libc_bionic_src_files_exclude_arm += \
+    arch-arm/generic/bionic/memcpy.S \
+    arch-arm/generic/bionic/memset.S \
+    arch-arm/generic/bionic/strcmp.S \
+    arch-arm/generic/bionic/strcpy.S \
+    arch-arm/generic/bionic/strlen.c \
+
+libc_bionic_src_files_arm += \
+    arch-arm/kryo/bionic/memcpy.S \
+    arch-arm/kryo/bionic/memmove.S \
+    arch-arm/krait/bionic/strcmp.S
+
+# Use cortex-a15 versions of strcat/strcpy/strlen and standard memmove
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a15/bionic/stpcpy.S \
+    arch-arm/cortex-a15/bionic/strcat.S \
+    arch-arm/cortex-a15/bionic/strcpy.S \
+    arch-arm/cortex-a15/bionic/strlen.S \
+
+# Use cortex-a53 memset
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a7/bionic/memset.S \
