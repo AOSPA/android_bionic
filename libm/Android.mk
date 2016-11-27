688dbbbdd8f90b1b467fd68f8911f7d69ffff0dd
Jake Weinstein <xboxlover360@gmail.com>
2016-11-27 15:17:26 -0500

    libm: add AArch32 math instructions
    Change-Id: I47d920802b2f5d60da2cf60166ac52324f29a7e8

--- libm/Android.mk
+++ libm/Android.mk
@@ -289,6 +289,36 @@ LOCAL_SRC_FILES_EXCLUDE_arm += \
     upstream-freebsd/lib/msun/src/e_sqrtf.c \
     upstream-freebsd/lib/msun/src/s_floor.c \
 
+ifneq (,$(filter cortex-a53 cortex-a53.a57 kryo denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+LOCAL_SRC_FILES_arm += \
+    arm/ceil.S \
+    arm/floor_aarch32.S \
+    arm/fmax.S \
+    arm/fmin.S \
+    arm/nearbyint.S \
+    arm/rint.S \
+    arm/round.S \
+    arm/trunc.S \
+
+LOCAL_SRC_FILES_EXCLUDE_arm += \
+    arm/floor.S \
+    upstream-freebsd/lib/msun/src/s_ceil.c \
+    upstream-freebsd/lib/msun/src/s_ceilf.c \
+    upstream-freebsd/lib/msun/src/s_floorf.c \
+    upstream-freebsd/lib/msun/src/s_fmax.c \
+    upstream-freebsd/lib/msun/src/s_fmaxf.c \
+    upstream-freebsd/lib/msun/src/s_fmin.c \
+    upstream-freebsd/lib/msun/src/s_fminf.c \
+    upstream-freebsd/lib/msun/src/s_nearbyint.c \
+    upstream-freebsd/lib/msun/src/s_rint.c \
+    upstream-freebsd/lib/msun/src/s_rintf.c \
+    upstream-freebsd/lib/msun/src/s_round.c \
+    upstream-freebsd/lib/msun/src/s_roundf.c \
+    upstream-freebsd/lib/msun/src/s_trunc.c \
+    upstream-freebsd/lib/msun/src/s_truncf.c \
+
+endif #AArch32 CPU Variants
+
 endif
 
 # -----------------------------------------------------------------------------
