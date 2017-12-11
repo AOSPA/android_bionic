ebefa56315be804d7f838c6e01e77c29940bd5fc
Jake Weinstein <jake@aospa.co>
2017-12-11 02:50:04 -0500

    Add remaining AArch32 and ARM64 builtin math routines
    Use builtins for nearbyint on both armv8 and arm64
    
    Use builtins for ceil/floor/rint/trunc on arm64
    
    Use builtins for fma/fmax/fmin/rint/round/trunc on 32-bit ARM
    All except fma/fmaf are only used for ARMv8 targets.
    
    Change-Id: I804211c259734a97ded4f240723f6dc30b106341

--- libm/builtins.cpp
+++ libm/builtins.cpp
@@ -45,9 +45,21 @@ long double fabsl(long double x) {
 }
 #endif
 
-#if defined(__aarch64__)
+// fma has builtin routines for ARMv7, ARMv8, and ARM64
+
+#if defined __arm__ || __aarch64__
 float fmaf(float x, float y, float z) { return __builtin_fmaf(x, y, z); }
 double fma(double x, double y, double z) { return __builtin_fma(x, y, z); }
+#endif
+
+// ceil/floor/fmax/fmin/rint/round/trunc has builtin routines for ARMv8 and ARM64
+
+#if defined __arm__ || __aarch64__
+float ceilf(float x) { return __builtin_ceilf(x); }
+double ceil(double x) { return __builtin_ceil(x); }
+
+float floorf(float x) { return __builtin_floorf(x); }
+double floor(double x) { return __builtin_floor(x); }
 
 float fmaxf(float x, float y) { return __builtin_fmaxf(x, y); }
 double fmax(double x, double y) { return __builtin_fmax(x, y); }
@@ -55,6 +67,28 @@ double fmax(double x, double y) { return __builtin_fmax(x, y); }
 float fminf(float x, float y) { return __builtin_fminf(x, y); }
 double fmin(double x, double y) { return __builtin_fmin(x, y); }
 
+float nearbyintf(float x) { return __builtin_nearbyintf(x); }
+double nearbyint(double x) { return __builtin_nearbyint(x); }
+long double nearbyintl(long double x) { return __builtin_nearbyint(x); }
+
+float rintf(float x) { return __builtin_rintf(x); }
+double rint(double x) { return __builtin_rint(x); }
+
 float roundf(float x) { return __builtin_roundf(x); }
 double round(double x) { return __builtin_round(x); }
+
+float truncf(float x) { return __builtin_truncf(x); }
+double trunc(double x) { return __builtin_trunc(x); }
+#endif
+
+#ifdef __arm__
+long double ceill(long double x) { return __builtin_fabsl(x); }
+
+long double floorl(long double x) { return __builtin_floorl(x); }
+
+long double fmal(long double a1, long double a2, long double a3) { return __builtin_fmal(a1, a2, a3); }
+
+long double rintl(long double x) { return __builtin_rintl(x); }
+
+long double truncl(long double x) { return __builtin_truncl(x); }
 #endif
