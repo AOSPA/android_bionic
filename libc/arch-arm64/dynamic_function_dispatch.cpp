26c714b24287bcb42adf951df3498f562887f7fb
Jake Weinstein <jake@aospa.co>
2022-02-20 13:53:07 +0900

    libc: arm64: integrate SVE routines
    Dynamically detect SVE using HWCAP.
    
    Switched static function dispatch to use the generic routines
    because some routines have MTE, some have SVE, but all have generic.
    This matches x86 and ARM.
    
    Change-Id: I7ae9834ca995d0be300f58f1912a67d2d28effe8

--- libc/arch-arm64/dynamic_function_dispatch.cpp
+++ libc/arch-arm64/dynamic_function_dispatch.cpp
@@ -44,6 +44,8 @@ DEFINE_IFUNC_FOR(memchr) {
 typedef void* memcpy_func(void*, const void*, size_t);
 DEFINE_IFUNC_FOR(memcpy) {
     if (arg->_hwcap & HWCAP_ASIMD) {
+        RETURN_FUNC(memcpy_func, __memcpy_aarch64_sve);
+    } else if (arg->_hwcap & HWCAP_ASIMD) {
         RETURN_FUNC(memcpy_func, __memcpy_aarch64_simd);
     } else {
         RETURN_FUNC(memcpy_func, __memcpy_aarch64);
@@ -53,6 +55,8 @@ DEFINE_IFUNC_FOR(memcpy) {
 typedef void* memmove_func(void*, const void*, size_t);
 DEFINE_IFUNC_FOR(memmove) {
     if (arg->_hwcap & HWCAP_ASIMD) {
+        RETURN_FUNC(memmove_func, __memmove_aarch64_sve);
+    } else if (arg->_hwcap & HWCAP_ASIMD) {
         RETURN_FUNC(memmove_func, __memmove_aarch64_simd);
     } else {
         RETURN_FUNC(memmove_func, __memmove_aarch64);
